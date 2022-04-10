4d0cf563642a9ded102327e57ea4b1c88dd4f34b
Ishtiaq uddin <ishty119@gmail.com>
2022-04-10 10:53:09 +0600

    form with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import { getAuth} from "firebase/auth"
-import './App.css';
-import app from './firebase.init';
+import { getAuth } from "firebase/auth";
+import "./App.css";
+import app from "./firebase.init";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
-
-  const handleEmailBlur = event => {
-    console.log(event.target.value)
-  }
-
-  const handlePasswordBlur = event => {
+  const handleEmailBlur = (event) => {
     console.log(event.target.value);
-  }
+  };
 
-  const handleFormSubmit = event =>{
-       console.log('from submitted');
-       event.preventDefault();
-  }
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    console.log("from submitted");
+    event.preventDefault();
+  };
   return (
-    <div className="App">
-      <form onSubmit={handleFormSubmit}>
-        <input onBlur={handleEmailBlur} type="email" name="" id="" />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
-        <br />
-        <input type="submit" value="LogIn" />
-      </form>
+    <div>
+      <div className="registration w-50 mx-auto mt-5">
+      <h2 className="text-primary">Please Register!!</h2>
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
     </div>
   );
 }
