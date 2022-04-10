8d3243e3b4bb5ab39f7d5e7bb7570800eec838b4
Ishtiaq uddin <ishty119@gmail.com>
2022-04-10 10:24:33 +0600

    input form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { getAuth} from "firebase/auth"
 import './App.css';
+import app from './firebase.init';
+
+const auth = getAuth(app)
 
 function App() {
   return (
     <div className="App">
-      
+      <form action="">
+        <input type="text" />
+        <input type="password" name="" id="" />
+      </form>
     </div>
   );
 }
